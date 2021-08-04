5975030b66f7dee95eabb11f81f5e5e5ae929748
Juan-Gabriel8 <juan.gabriel.m710@gmail.com>
2021-08-04 17:36:21 -0300

    DataConfiguration

---
+++ src/main/java/com/spring/techplus/TechplusApplication.java
@@ -0,0 +1,13 @@
+package com.spring.techplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechplusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechplusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/techplus/TechplusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.techplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechplusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
