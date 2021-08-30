b9c1ce068577780837dfcae888eb44dd88e7bf35
Juan-Gabriel8 <juan.gabriel.m710@gmail.com>
2021-08-30 00:32:31 -0300

    Criação do formulário para geração de novos posts

--- src/main/java/com/spring/techplus/controller/TechplusController.java
+++ src/main/java/com/spring/techplus/controller/TechplusController.java
@@ -1,32 +1,37 @@
 package com.spring.techplus.controller;
 
+import java.time.LocalDate;
 import java.util.List;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.spring.techplus.model.Post;
 import com.spring.techplus.service.TechplusService;
 
 @Controller
 public class TechplusController {
-	
+
 	@Autowired
 	TechplusService techplusService;
-	
+
 	@RequestMapping(value = "/posts", method = RequestMethod.GET)
 	public ModelAndView getPosts() {
 		ModelAndView mv = new ModelAndView("posts");
 		List<Post> posts = techplusService.findAll();
 		mv.addObject("posts", posts);
 		return mv;
-		
+
 	}
-	
+
 	@RequestMapping(value = "/posts/{id}", method = RequestMethod.GET)
 	public ModelAndView getPostDetails(@PathVariable("id") long id) {
 		ModelAndView mv = new ModelAndView("postDetails");
@@ -34,4 +39,20 @@ public class TechplusController {
 		mv.addObject("post", post);
 		return mv;
 	}
+
+	@RequestMapping(value = "/newpost", method = RequestMethod.GET)
+	public String getPostForm() {
+		return "postForm";
+	}
+
+	@RequestMapping(value = "/newpost", method = RequestMethod.POST)
+	 public String savePost(@Valid Post post, BindingResult result, RedirectAttributes attributes){
+        if(result.hasErrors()){
+            attributes.addFlashAttribute("mensagem", "Verifique se os campos obrigatórios foram preenchidos!");
+			return "redirect:/newpost";
+		}
+        post.setData(LocalDate.now());
+        techplusService.save(post);
+        return "redirect:/posts";
+	 } 
 }
